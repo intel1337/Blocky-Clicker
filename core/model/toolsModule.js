5ad4776e9d3c14c40be227345f0b5eb4ea163933
intel <93615583+intel1337@users.noreply.github.com>
2025-02-18 12:47:27 +0100

    Delete core/model directory

--- core/model/toolsModule.js
+++
@@ -1,4 +0,0 @@
-import { Shovel } from "../../public/src/model/tools/class/Shovel.js";
-import { Axe } from "../../public/src/model/tools/class/Axe.js";
-import { Pickaxe } from "../../public/src/model/tools/class/Pickaxe.js";
-import { Sword } from "../../public/src/model/tools/class/Sword.js";
--- core/model/worldsModule.js
+++
@@ -1,5 +0,0 @@
-import { Cave } from "../../public/src/model/worlds/class/Cave.js";
-import { Mine1 } from "../../public/src/model/worlds/class/Mine1.js";
-import { Mine2 } from "../../public/src/model/worlds/class/Mine2.js";
-import { Filon } from "../../public/src/model/worlds/class/Filon.js";
-import { Nether } from "../../public/src/model/worlds/class/Nether.js";
